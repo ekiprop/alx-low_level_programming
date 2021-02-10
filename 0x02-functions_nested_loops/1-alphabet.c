c5d1bc77eee63dd0319936349df9adc8db339d77
Kiprop <evanskiprop1@gmail.com>
2021-02-10 09:11:23 +0300

    task 1 and 4 correcton

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
-  *print_alphabet - Prints alphabet
+ * print_alphabet - Prints alphabet
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a'; x <= 'z'; c++)
-		_putchar(c)
-	_putchar('\n)
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * _isalpha - function checks for both lowercase and uppercase character
- * @c: The character to check
+ * _isalpha - Checks for alphabetic character
+ * @c: Character to check
  *
  * Return: 1 if matches, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 65) && (c <= 90) || (c >= 97) && (c <=122))
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
 		return (1);
 	return (0);
 }
