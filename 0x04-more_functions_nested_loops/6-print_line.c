e9b2f88cb62d3b783671f3d9b7258ed0c61aed44
Kiprop <evanskiprop1@gmail.com>
2021-02-11 19:18:53 +0300

    task 0 to 10

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_line - prints a straight line
+ * @n: number in use
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (n == 0)
+{
+	_putchar('\n')
+}
+else
+{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_')
+	}
+	_putchar('\n')
+}
+
+}
